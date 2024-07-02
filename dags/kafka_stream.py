ce25806dc5302f8713d3c88221a16f973bb5d564
Mohammed Raza Syed <mohdraza.syed@gmail.com>
2024-07-02 21:02:50 +0530

    spark streaming code added

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import json
+import uuid
 import requests
 from kafka import KafkaProducer
 import time
@@ -12,19 +13,23 @@ default_args = {
 }
 
 def get_data():
-    res = requests.get('https://randomuser.me/api/')
+    import requests
+
+    res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
+
     return res
 
 def format_data(res):
     data = {}
     location = res['location']
+    data['id'] = uuid.uuid4()
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
     data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
-                        f"{location['city']}, {location['state']}, {location['country']}"
+                      f"{location['city']}, {location['state']}, {location['country']}"
     data['post_code'] = location['postcode']
     data['email'] = res['email']
     data['username'] = res['login']['username']
@@ -32,27 +37,24 @@ def format_data(res):
     data['registered_date'] = res['registered']['date']
     data['phone'] = res['phone']
     data['picture'] = res['picture']['medium']
-    
+
     return data
 
 def stream_data():
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-    # print(json.dumps(data, indent=4))
-
-    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)    
-    
-    current_time = time.time()
     while True:
-        if time.time() > current_time + 60:
+        if time.time() > curr_time + 60: #1 minute
             break
         try:
             res = get_data()
-            data = format_data(res)
-            
-            producer.send('users_created', json.dumps(data).encode('utf-8'))
-        
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
         except Exception as e:
-            logging.error('An error occudred: ', e)
+            logging.error(f'An error occured: {e}')
+            continue
 
 dag = DAG(
     dag_id='random_names_kafka_stream',
